79ee5a1865356b419be712cf5b07b1b2c32731a4
xiaguang <1484073720@qq.com>
2018-04-02 16:37:16 +0800

    two

--- iotek-config-server/src/main/java/iotek/config/server/IotekConfigServerApplication.java
+++ iotek-config-server/src/main/java/iotek/config/server/IotekConfigServerApplication.java
@@ -2,8 +2,11 @@ package iotek.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class IotekConfigServerApplication {
